46c1925dadff616e9a55b774f977983c3d2a2dbf
Marc Scheib <marc.scheib@gmx.de>
2015-12-12 11:40:14 +0100

    feat(profile): add auth rules to profile controller
    Profile controller is accessible only as a logged in user now. The
    Apache Server needs a rewrite rule to save the authentication token.
    
    ---
    
     app/Http/Controllers/ProfileController.php | 5 +++++
     app/Http/Kernel.php                        | 2 ++
     public/.htaccess                           | 4 ++++
     3 files changed, 11 insertions(+)

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -5,6 +5,11 @@ use LegendsRising\Models\User;
 
 class ProfileController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('jwt.auth');
+    }
+
     public function show($id)
     {
         return User::find($id);
--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -29,5 +29,7 @@ class Kernel extends HttpKernel
         'auth' => \LegendsRising\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'guest' => \LegendsRising\Http\Middleware\RedirectIfAuthenticated::class,
+        'jwt.auth' => \Tymon\JWTAuth\Middleware\GetUserFromToken::class,
+        'jwt.refresh' => \Tymon\JWTAuth\Middleware\RefreshToken::class,
     ];
 }
