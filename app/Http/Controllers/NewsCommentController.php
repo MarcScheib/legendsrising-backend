24bd75175f5ebeee572fd47c3db69d8684b24151
Marc Scheib <marc.scheib@gmx.de>
2016-02-06 14:57:53 +0100

    feat(news): add comment store to news resource
    Add a method to store new comments for a specific news.
    
    ---
    
     app/Http/Controllers/NewsCommentController.php | 27 ++++++++++++++++++-
     1 file changed, 26 insertions(+), 1 deletion(-)

--- app/Http/Controllers/NewsCommentController.php
+++ app/Http/Controllers/NewsCommentController.php
@@ -2,14 +2,39 @@
 
 namespace LegendsRising\Http\Controllers;
 
+use Auth;
+use Illuminate\Http\Request;
 use LegendsRising\Models\Comment;
 
 class NewsCommentController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('jwt.auth', ['except' => ['index']]);
+    }
+
     public function index($newsId)
     {
         $comments = Comment::recent()->with('user')->where('news_id', $newsId)->get();
 
-        return $comments;
+        return $comments->reverse();
+    }
+
+    public function store($newsId, Request $request)
+    {
+        $this->validate($request, [
+            'text' => 'required'
+        ]);
+
+        $user = Auth::user();
+        $comment = new Comment();
+        $comment->news_id = $newsId;
+        $comment->user_id = $user->id;
+        $comment->text = $request->text;
+        $comment->save();
+
+        $comment->user = $user;
+
+        return $comment;
     }
 }
